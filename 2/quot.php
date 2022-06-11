c96cea8454347581da3bee318ca030e63d08185c
kim-sungmin <ksm981204@naver.com>
2022-06-11 23:27:05 +0900

    upload quot.php

---
+++ 2/quot.php
@@ -0,0 +1,21 @@
+<?php
+    $name = "홍지수";
+    echo $name;
+    echo "<br>";
+
+    echo $name;
+    echo "<br>";
+
+    echo "$name <br>";    
+
+    echo '$name <br>';  //$name 출력
+
+    echo "$name님 <br>";    //name님 으로 인식 에러
+
+    echo "{$name}님 <br>";    
+
+    //echo ""$name" <br>";    //에러
+    
+    echo "\"$name\" <br>";    
+?>
+    <?=$name?>
