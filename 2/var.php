dda42332910ef58bfad225b084358d4021f08089
kim-sungmin <ksm981204@naver.com>
2022-06-11 23:02:56 +0900

    upload var.php

---
+++ 2/var.php
@@ -0,0 +1,20 @@
+<?php 
+    $a = 10;
+    $b = 20;
+    $c = $a+$b;
+    echo "$c <br>";
+
+    $a = 18.5;
+    $b =29.2;
+    $c = $a+$b;
+    echo "$c <br>";
+
+    $a = 10;
+    $b =29.5;
+    $c = $a+$b;
+    echo "$c <br>";
+
+    $fruit = "사과";
+    echo "$fruit <br>";
+
+?>
