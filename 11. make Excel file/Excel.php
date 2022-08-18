c99d77ebf0825ce4bb45e9fe00c4f7539492f1f4
kim-sungmin <ksm981204@naver.com>
2022-08-18 12:44:57 +0900

    add kor, eng

--- 11. make Excel file/Excel.php
+++ 11. make Excel file/Excel.php
@@ -1,12 +1,12 @@
 <?php
     $file_name ="testing";
-    header("Content-Disposition: attachment; filename=$file_name.xls");
+    header("Content-Disposition: attachment; charset=utf-16;filename=$file_name.xls");
 ?>
 <table border="1">
     <tr height="60" align="center" valign="middle">
         <td width="60"> 가 </td>
-        <td width="60"> 나 </td>
-        <td width="60"> 다 </td>
+        <td width="60"> B </td>
+        <td width="60"> C </td>
     </tr>
     <tr height="60" align="center" valign="middle">
         <td width="60"> 1 </td>
